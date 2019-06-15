2dcb3fed74a45b12a3b100255ba7111980a7b826
Clément Garnier <cle.garnier@gmail.com>
2019-06-15 10:38:48 +0200

    Fix exemple
    Update search query

--- examples/duckduckgo/index.js
+++ examples/duckduckgo/index.js
@@ -3,7 +3,7 @@ var nightmare = Nightmare({ show: false });
 
 nightmare
   .goto('https://duckduckgo.com')
-  .type('#search_form_input_homepage', 'github nightmare')
+  .type('#search_form_input_homepage', 'nightmare js')
   .click('#search_button_homepage')
   .wait('#zero_click_wrapper .c-info__title a')
   .evaluate(function () {
